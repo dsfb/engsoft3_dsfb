a989802ab40c8696ed12d499e8897263942cc395
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2018-06-12 23:48:02 -0300

    Começo da implementação da tela principal pós-login!

---
+++ engsoft3/FormMainScreen.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace engsoft3
+{
+    partial class FormMainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateGame = new System.Windows.Forms.Button();
+            this.btnRankings = new System.Windows.Forms.Button();
+            this.btnFindGame = new System.Windows.Forms.Button();
+            this.btnChooseSkin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateGame
+            // 
+            this.btnCreateGame.Location = new System.Drawing.Point(126, 114);
+            this.btnCreateGame.Name = "btnCreateGame";
+            this.btnCreateGame.Size = new System.Drawing.Size(97, 23);
+            this.btnCreateGame.TabIndex = 0;
+            this.btnCreateGame.Text = "Criar Jogo";
+            this.btnCreateGame.UseVisualStyleBackColor = true;
+            // 
+            // btnRankings
+            // 
+            this.btnRankings.Location = new System.Drawing.Point(376, 114);
+            this.btnRankings.Name = "btnRankings";
+            this.btnRankings.Size = new System.Drawing.Size(92, 23);
+            this.btnRankings.TabIndex = 1;
+            this.btnRankings.Text = "Rankings";
+            this.btnRankings.UseVisualStyleBackColor = true;
+            // 
+            // btnFindGame
+            // 
+            this.btnFindGame.Location = new System.Drawing.Point(126, 175);
+            this.btnFindGame.Name = "btnFindGame";
+            this.btnFindGame.Size = new System.Drawing.Size(97, 23);
+            this.btnFindGame.TabIndex = 2;
+            this.btnFindGame.Text = "Encontrar Jogo";
+            this.btnFindGame.UseVisualStyleBackColor = true;
+            // 
+            // btnChooseSkin
+            // 
+            this.btnChooseSkin.Location = new System.Drawing.Point(376, 175);
+            this.btnChooseSkin.Name = "btnChooseSkin";
+            this.btnChooseSkin.Size = new System.Drawing.Size(92, 23);
+            this.btnChooseSkin.TabIndex = 3;
+            this.btnChooseSkin.Text = "Escolher Skins";
+            this.btnChooseSkin.UseVisualStyleBackColor = true;
+            // 
+            // FormMainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 261);
+            this.Controls.Add(this.btnChooseSkin);
+            this.Controls.Add(this.btnFindGame);
+            this.Controls.Add(this.btnRankings);
+            this.Controls.Add(this.btnCreateGame);
+            this.Name = "FormMainScreen";
+            this.Text = "FormMainScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateGame;
+        private System.Windows.Forms.Button btnRankings;
+        private System.Windows.Forms.Button btnFindGame;
+        private System.Windows.Forms.Button btnChooseSkin;
+    }
+}
---
+++ engsoft3/FormMainScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace engsoft3
+{
+    public partial class FormMainScreen : Form
+    {
+        public FormMainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
